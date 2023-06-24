bf56beb13157dfa67994a1dce3ed90913619ec32
Taehyun Kim <agart319@gmail.com>
2023-06-24 16:27:19 +0900

    오타수정

--- src/components/TodoListItems.js
+++ src/components/TodoListItems.js
@@ -10,7 +10,7 @@ const TodoListItems = ({ todo, key, onChangeHandler, onDeleteHandler }) => {
             <h4>{body}</h4>
             <div>
                 <button className='items-delete' onClick={() => { onDeleteHandler(id) }}>삭제하기</button>
-                <button className='items-done' onClick={() => { onChangeHandler(id) }}>{isDone ? '완료 취소' : '완료'}완료</button>
+                <button className='items-done' onClick={() => { onChangeHandler(id) }}>{isDone ? '완료 취소' : '완료'}</button>
             </div>
         </article>
     )
